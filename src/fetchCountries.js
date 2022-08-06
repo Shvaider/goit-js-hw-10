1956e1aa98b755000a6be5fab725526cde8a7f24
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-08-06 09:00:39 +0300

    save06.08

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+const fetchCountries = (name) => {
+    const DATA_URL = 'https://restcountries.com/v3.1/name/{name}'
+    let url = `${DATA_URL}${name}`
+    return fetch(url)
+    .then(response => response.json())
+    .catch(error => console.log('error'))
+}
+export default fetchCountries
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import { debounce } from 'lodash';
+
+
+
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const onInputChange = (e) => {
+    e.preventDefault()
+
+    refs.countryList.innerHTML = ''
+
+    const inputValue = e.target.value;
+
+    if(inputValue){
+        fetchCountries(search-box.trim()).then(data => updateMarkup(data)).catch(error => console.log('error'))
+    }
+}
+
+function updateMarkup (data) {
+const markup = templates(data)
+
+if(!data.length) {
+    error({ })
+}
+}
